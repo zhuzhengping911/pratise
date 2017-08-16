ac29911d93ff2623abcf27f9e8cd627d50956da8
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-08-17 01:06:51 +0800

    final

---
+++ src/main/java/com/zzp/pratise/reflect/main/FinalModify.java
@@ -0,0 +1,12 @@
+package com.zzp.pratise.reflect.main;
+
+/**
+ * Created by zhuzhengping
+ * on 2017/8/17.
+ */
+public class FinalModify {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/zzp/pratise/reflect/pojo/FinalClass.java
@@ -0,0 +1,27 @@
+package com.zzp.pratise.reflect.pojo;
+
+/**
+ * Created by zhuzhengping
+ * on 2017/8/17.
+ */
+public class FinalClass {
+
+    private final String FINAL_VALUE = "FINAL";
+
+    private final String FINAL_VALUE_MODIFY = "MODIFY";
+
+    private final String FINAL_VALUE_CHOSE = "CHOSE";
+
+
+    public String getFINAL_VALUE() {
+        return FINAL_VALUE;
+    }
+
+    public String getFINAL_VALUE_MODIFY() {
+        return FINAL_VALUE_MODIFY;
+    }
+
+    public String getFINAL_VALUE_CHOSE() {
+        return FINAL_VALUE_CHOSE;
+    }
+}
