3d3c03b41fd7a649b62ca992a8aa6a51ef88b68a
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-08-18 00:17:43 +0800

    proxy

---
+++ src/main/java/com/zzp/pratise/proxy/pojo/TargetImpl.java
@@ -0,0 +1,15 @@
+package com.zzp.pratise.proxy.pojo;
+
+/**
+ * Created by zhuzhengping
+ * on 2017/8/17.
+ */
+public class TargetImpl implements Target{
+
+    @Override
+    public String excute() {
+
+        System.out.println("TargetImpl execute！");
+        return "execute";
+    }
+}
