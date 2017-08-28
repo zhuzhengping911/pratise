447893ef9ffc4ed97106c9f079ec491137f0ad95
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-08-29 00:43:31 +0800

    Test MyMap

--- src/main/java/com/zzp/pratise/myMap/MyMapTest.java
+++ src/main/java/com/zzp/pratise/myMap/MyMapTest.java
@@ -1,5 +1,7 @@
 package com.zzp.pratise.myMap;
 
+import com.zzp.pratise.myMap.pojo.Person;
+
 /**
  * Created by zhuzhengping
  * on 2017/8/28.
@@ -16,5 +18,20 @@ public class MyMapTest {
 
         System.out.println(map.size());
         System.out.println(map.get("zzp2"));
+
+        MyMap<String,Person> personMyMap = new MyMap<String, Person>();
+        personMyMap.put("First",new Person("Tom",25,"England"));
+        personMyMap.put("Second",new Person("Eddie",26,"American"));
+        personMyMap.put("Third",new Person("Marry",38,"China"));
+        personMyMap.put("Fouth",new Person("Edward",12,"Japan"));
+        personMyMap.put("Fifth",new Person("Jack",65,"Canada"));
+
+        System.out.println("Total Person" + personMyMap.size());
+
+        for (int index = 0 ; index < personMyMap.size(); index++){
+            System.out.println("Person name:" + personMyMap.get(index).getName() + "\tPerson Age:" + personMyMap.get(index).getAge() + "\tPerson Coutry:" + personMyMap.get(index).getCountry());
+        }
+
+        System.out.println(personMyMap.get(6));
     }
 }
