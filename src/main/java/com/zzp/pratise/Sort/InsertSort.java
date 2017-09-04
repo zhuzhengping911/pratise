608467119b74cc908e1359b2703cbef401171a40
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-09-04 22:35:19 +0800

    Sort

---
+++ src/main/java/com/zzp/pratise/Sort/InsertSort.java
@@ -0,0 +1,23 @@
+package com.zzp.pratise.Sort;
+
+/**
+ * Created by zhuzhengping
+ * on 2017/9/4.
+ */
+public class InsertSort {
+
+    public void insertSort(int[] a){
+        int length=a.length;//数组长度，将这个提取出来是为了提高速度。
+        int insertNum;//要插入的数
+        for(int i=1;i<length;i++){//插入的次数
+            insertNum=a[i];//要插入的数
+            int j=i-1;//已经排序好的序列元素个数
+            while(j>=0&&a[j]>insertNum){//序列从后到前循环，将大于insertNum的数向后移动一格
+                a[j+1]=a[j];//元素移动一格
+                j--;
+            }
+            a[j+1]=insertNum;//将需要插入的数放在要插入的位置。
+        }
+    }
+    
+}
